76b1b5db9e54e9e2598e05e3dd85ccc0611d3497
KRIDSAYA NATEEMONTON <63010026@KMITL.AC.TH>
2020-08-23 08:36:54 +0700

    Add project files.

---
+++ lab02 - 03/lab02-03.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int row, i, j;
+
+    printf("Enter number : ");
+    scanf_s("%d", &row);
+
+    for (i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= row; j++)
+        {
+            if (i == 1 || i == row || j == 1 || j == row)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
